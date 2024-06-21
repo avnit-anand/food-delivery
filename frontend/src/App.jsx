8f6b0def2e12d08e9fcd82f2670c7bbe0974902d
avnit-anand <avnitanand1806@gmail.com>
2024-06-21 15:16:05 +0530

    Navbar, Header, Category Menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 
 const App = () => {
   return (
-    <div>
-      <h1>app</h1>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./PlaceOrder.css";
+const PlaceOrder = () => {
+  return (
+    <div>
+      <h1>placeOrder</h1>
+    </div>
+  );
+};
+
+export default PlaceOrder;
